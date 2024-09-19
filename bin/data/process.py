d22617af3dfaeaedcfc708e5da8501d27ac59e96
zam <azamolot@tcd.je>
2024-09-19 19:49:08 +0100

    Cleanup

--- bin/data/process.py
+++ bin/data/process.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def get_data(inputdir):
     df = pd.read_csv(inputdir)
     return df
--- bin/data/snippet.py
+++ bin/data/snippet.py
@@ -1,14 +1,13 @@
-import pandas as pd
 import random
 
+
 def individual_snippets(row):
-    rand1 = random.randint(1,int(len(row)/2))
-    rand2 = random.randint(int(len(row)/2), len(row)-2)
-    row = "<fim_prefix>" +row[:rand1] + "<fim_suffix>" + row[rand2:] + "<fim_middle>"
+    rand1 = random.randint(1, int(len(row) / 2))
+    rand2 = random.randint(int(len(row) / 2), len(row) - 2)
+    row = "<fim_prefix>" + row[:rand1] + "<fim_suffix>" + row[rand2:] + "<fim_middle>"
     return row
 
 
 def create_snippets(df):
     df["processed"] = df["raw"].apply(individual_snippets)
     return df
-
