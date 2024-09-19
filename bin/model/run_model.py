99d138d012d3ddfc1f84f39d2b582a92da5aa652
zam <azamolot@tcd.je>
2024-09-19 20:27:08 +0100

    Model Development

--- bin/model/run_model.py
+++ bin/model/run_model.py
@@ -3,22 +3,35 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 class SnippetModel:
     def __init__(self, name, device):
-        self.raw_data = None
-        self.tokenized_data = None
-        self.output_data = None
+        self.data = None
         self.device = device
         self.tokenizer = AutoTokenizer.from_pretrained(name)
         self.model = AutoModelForCausalLM.from_pretrained(name).to(device)
 
     def insert_data(self, data):
-        self.data = data
+        self.data = data.copy()
+
+    def tokenize_one(self, row):
+        return self.tokenizer.encode(row, return_tensors="pt").to(self.device)
 
     def tokenize_data(self):
-        self.tokenized_data = self.tokenizer.encode(self.raw_data, return_tensors="pt").to(self.device)
+        print(self.data["processed"])
+        self.data["tokenized"] = self.data["processed"].apply(self.tokenize_one)
+        print(self.data)
+
+    def run_model_once(self, row):
+        return self.model.generate(row, max_length=100)
+
     def run_model(self):
-        self.output_data = self.model.generate(self.tokenize_data())
+        self.data["output"] = self.data["tokenized"].apply(self.run_model_once)
+
+    def decode_one(self, row):
+        return self.tokenizer.decode(row[0])
+
+    def decode_data(self):
+        self.data["decoded_output"] = self.data["output"].apply(self.decode_one)
 
     def get_output(self):
-        return self.output_data
+        return self.data["decoded_output"]
 
 
